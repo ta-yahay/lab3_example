b6cec4e6f23fa62eedb6bced7b5eccb5e8aba9b7
yahyakhalaf99 <yahya.ahmed99@eng-st.cu.edu.eg>
2024-03-06 00:33:02 +0200

    first commit

--- azureproject/development.py
+++ azureproject/development.py
@@ -1,5 +1,7 @@
 import os
 
+conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 
 DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
     dbuser=os.environ['DBUSER'],
